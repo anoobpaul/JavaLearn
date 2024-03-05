19cea50258e3c8dad480491b60366ae579291810
Anoob Paul <155704736+anoobpaul@users.noreply.github.com>
2024-03-05 22:29:16 +0530

    Update MajorityElement.java
    comeent section added

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,3 +1,10 @@
+/*169. Majority Element
+ * Given an array nums of size n, return the majority element.
+  The majority element is the element that appears more than ⌊n / 2⌋ times. 
+  You may assume that the majority element always exists in the array.
+ * 
+ */
+
 public class MajorityElement {
 
     public int findMajourElement(int nums[]) {
@@ -23,7 +30,7 @@ public class MajorityElement {
 
         MajorityElement me = new MajorityElement();
 
-        int nums[] = { 3, 2, 2 };
+        int nums[] = { 2, 2, 3 };
         int result = me.findMajourElement(nums);
         System.out.println("Result is " + result);
 
